5cdb1b7c070aa7d2fd0dc3bc5f9a639ca6eeb272
GretelDePaepe <gretel_depaepe@me.com>
2017-04-01 09:25:52 +0400

    Major new releases, using pickled files to look through coordinates going up by 0.001, while staying below 24 request limit

--- GooglePlacesToMongo.py
+++ GooglePlacesToMongo.py
@@ -11,26 +11,9 @@ import urllib
 import json
 from pymongo import MongoClient
 import datetime as dt
+import pickle
 
 # %% The functions
-
-
-def create_coordinates_list(lat_min, lat_max, lon_min, lon_max):
-    """
-    Creates a list with all the coordinate pairs incrementing
-    the min values by 0.01 until the max values are reached.
-    """
-    increment = 0.01
-    all_coordinates = []
-    lat = lat_min - increment
-    lon = lon_min - increment
-    for no_of_lats in range(int((lat_max - lat_min) / increment)):
-        lat += increment
-        lon = lon_min - increment  # lon needs to be reset before 2nd loop!
-        for no_of_lons in range(int((lon_max - lon_min) / increment)):
-            lon += increment
-            all_coordinates.append((round(lat, 2), round(lon, 2)))
-    return all_coordinates
 
 
 def google_places(lat, lon, radius, types, key):
@@ -39,6 +22,13 @@ def google_places(lat, lon, radius, types, key):
     information on a variety of categories, such as: establishments,
     prominent points of interest, geographic locations, and more.
     https://developers.google.com/places/web-service/search
+    lat = latitude in decimal form eg. 47.573
+    lon = longitude in decimal form eg. -122.284
+    radius = Defines the distance (in meters) within which to return place
+             results. The maximum allowed radius is 50 000 meters.
+    types = Restricts the results to places matching the specified type,
+            eg. 'restaurant', 'bar', etc.
+    key = user's API key
     """
     location = str(lat) + "," + str(lon)
     my_url = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
@@ -58,6 +48,9 @@ def google_places_next(page_token, key):
     more a next_page token.  The max is 60 results, 20 at a time.  This
     function looks for the next page token and if it exist, gets the next
     20 results twice.
+    page_token = key returned by Google Places API in case there are more
+                 results which can be requested.
+    key = user's API key
     """
     my_url = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
               '?pagetoken=%s'
@@ -68,11 +61,13 @@ def google_places_next(page_token, key):
     return json_data
 
 
-def get_cats(path):
+def get_types(types_path_file):
     """
     Loads a list of categories we will search for, from a file
+    types_path_file = path and file name of the file which contains the types
+                      we are going to search for, eg. 'restaurant', 'bar', etc.
     """
-    file_path = path
+    file_path = types_path_file
     file_open = open(file_path, 'r')
     file_read = file_open.read()
     cats = file_read.split(',\n')
@@ -82,36 +77,52 @@ def get_cats(path):
 def connect_mongo(database):
     """
     Creates a connection to a mongo database
+    database = name of the database
     """
     client = MongoClient()
     db = eval('client.%s' % database)
     return db
 
 
-def store_in_mongo(db, collection, google_data, tc):
+def store_in_mongo(db, collection, google_data, tc, hood):
     """
     Loops through the results returned by the Google Places API Web Service
     and adds each doc to a mongo collection.
     Adds a date tag to the doc.
+    db = mongo database connection
+    collection = name of the mongo collection
+    google_data = data returned from the Google Places API
+    tc = today's date
+    hood = neighborhood
     """
     docs = google_data['results']
     for no_of_docs in range(len(docs)):
         doc = docs[no_of_docs]
         doc['date'] = tc
-        eval("db.%s.insert_one(doc)" % collection)
+        doc['hood'] = hood
+        place = doc['place_id']
+        con = "db.%s.find({'place_id': '%s', 'date': '%s'}).count()" % (collection, place, tc)
+        count = eval(con)
+        if count == 0:
+            eval("db.%s.insert_one(doc)" % collection)
 
 
-def create_daily_count(db, collection, tc, path):
+def create_daily_count(db, collection, tc, path_file):
     """
     Connect to a mongo database and counts the docs in a collection
     with a specific date tag and stores the date and count in a csv file.
+    db = mongo database connection
+    collection = name of the mongo collection
+    tc = today's date
+    path_file = path and the file to save to
     """
     con = "db.%s.find({'date': '%s'}).count()" % (collection, tc)
     count = eval(con)
-    f = open(path, 'a')
+    f = open(path_file, 'a')
     f.write(tc + "," + str(count) + "\n")
     f.close()
     return count
+
 
 # %% The program
 
@@ -119,38 +130,58 @@ def create_daily_count(db, collection, tc, path):
 def main():
 
     start = dt.datetime.now()
+    number_of_requests = 0
+    max_number_of_requests = 140000  # max number of requests in 24 hours is 150,000
+    max_number_of_requests = 500
+
+    path = '/home/ec2-user/pythonscripts/'
+
+    bookmark_pickle_file = "bookmark.p"
+    bookmark = pickle.load(open(path + bookmark_pickle_file, "rb"))
 
     db = connect_mongo('FindingDonuts')
     collection = 'SeattleGoogle'
 
-    key = "TYPE YOUR OWN"
-    radius = 5000
+    key = "AIzaSyCdVApz2Qw50B62TKOOCQkH1Y3dAJM7A5M"
+    radius = 100
 
     t = dt.datetime.now()
     tc = t.strftime('%m/%d/%Y')
 
-    cats = get_cats('/home/ec2-user/pythonscripts/GoogleTypes.csv')
-    all_coord = create_coordinates_list(47.48, 47.75, -122.43, -122.20)
+    coordinates_pickle_file = "neighborhood_coordinates.p"
+    seattle_coordinates = pickle.load(open(path+coordinates_pickle_file, "rb"))
 
-    for cat in cats:
-        for lat, lon in all_coord:
-            google_data = google_places(lat, lon, radius, cat, key)
-            store_in_mongo(db, collection, google_data, tc)
+    types_file = 'GoogleTypes.csv'
+    types = get_types(path + types_file)
+
+    while number_of_requests < max_number_of_requests:
+        hood, lat, lon = seattle_coordinates[bookmark]
+        print "--", bookmark, hood, lat, lon, number_of_requests
+        for one_type in types:
+            google_data = google_places(lat, lon, radius, one_type, key)
+            store_in_mongo(db, collection, google_data, tc, hood)
+            number_of_requests += 1
             for j in range(2):
                 try:
                     pagetoken = google_data['next_page_token']
+                    number_of_requests += 1
                 except:
                     pagetoken = "No more"
                 if pagetoken != "No more":
                     google_data = google_places_next(pagetoken, key)
-                    store_in_mongo(db, collection, google_data, tc)
+                    store_in_mongo(db, collection, google_data, tc, hood)
+                    number_of_requests += 1
+        if bookmark < 27337:
+            bookmark += 1
+        else:
+            bookmark = 0
+
+    pickle.dump(bookmark, open(path + bookmark_pickle_file, "wb"))
+
+    dc_file = 'GoogleCountPerDay.csv'
+    create_daily_count(db, collection, tc, path + dc_file)
 
     end = dt.datetime.now()
-
-    file_dc = 'GoogleCountPerDay.csv'
-    path_dc = '/home/ec2-user/pythonscripts/' + file_dc
-    create_daily_count(db, collection, tc, path_dc)
-
     print start, end
 
 # %% Standard boilerplate to call the main() function
